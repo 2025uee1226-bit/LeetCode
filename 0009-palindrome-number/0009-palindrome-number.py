f3a1f3ae2b69aa0e503073047c328c5fee3a2d5d
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-04 23:46:15 +0530

    Time: 6 ms (81.03%), Space: 12.3 MB (53.86%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        p=x
+        c=0
+    
+        while(x>0):
+            c= c*10+x%10
+            x=x//10
+        return c==p
+            
