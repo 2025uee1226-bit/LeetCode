14a365ae52f87625fbfa3b00d08a2d92cdb4c972
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-08 00:24:33 +0530

    Time: 6 ms (28.69%), Space: 12.4 MB (28.61%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        j=0
+        i=0
+        s=sorted(s)
+        t=sorted(t)
+        while(i<len(s)):
+            if s[i]!=t[j]:
+                return t[j]
+                break
+                
+            else:
+                j+=1
+                i+=1
+        j+=1
+        if j>i:
+            return t[-1]
