729c25341c427e8ed6a1e006f8c6d07904e3ee27
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-07 23:40:19 +0530

    Time: 234 ms (69.9%), Space: 22.1 MB (65.67%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        m=0
-        i=0
+        
+
         p=0
+        m=0
         nums.sort()
         for j in range(1,len(nums)):
-            i=j-1
-            p=nums[j]-nums[i]
+            p=nums[j]-nums[j-1]
             m=max(m,p)
         return m
