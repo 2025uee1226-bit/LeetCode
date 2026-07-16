e0339f55276a7e7a12b0cccf8495d2b285975335
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-16 09:00:54 +0530

    Time: 40 ms (19.81%), Space: 13.2 MB (5.95%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -7,6 +7,7 @@ class Solution(object):
         u=0
         d=0
         r,l=0,0
+    
         for i in range(len(moves)):
             if moves[i]=="U":
                 u+=1
@@ -16,5 +17,6 @@ class Solution(object):
                 r+=1
             else:
                 l+=1
-        return u==d and r==l
+        f= (u==d and r==l)
+        return f
         
