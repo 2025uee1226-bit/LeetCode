0a84b397ba16922f68b98e10ca9a16e6b26e69fd
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-21 00:39:42 +0530

    Time: 67 ms (27.3%), Space: 12.4 MB (89.33%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countDigitOccurrences(self, nums, digit):
+        """
+        :type nums: List[int]
+        :type digit: int
+        :rtype: int
+        """
+        c=0
+        for num in nums:
+            for d in str(num):
+                if d==str(digit):
+                    c+=1
+        return c
