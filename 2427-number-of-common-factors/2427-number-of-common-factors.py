aa559d3989c5a6975cff401f76baf7385bd5bcec
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-26 00:01:41 +0530

    Time: 2 ms (34.95%), Space: 12.2 MB (88.84%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        sum=0
+        for i in range(1,min(a,b)+1):
+            if a%i==0 and b%i==0:
+                sum+=1
+        return sum
+        
