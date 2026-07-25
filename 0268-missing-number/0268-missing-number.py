3e3f215e67c81bbb406b0ca4e7da17c2a7b1355c
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-25 23:23:15 +0530

    Time: 3 ms (56.29%), Space: 13.4 MB (35.5%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -3,10 +3,14 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        """
+        
         sum=0
         n=len(nums)
         for i in range (0,n) :
             sum=sum+nums[i]
-        return n*(n+1)//2-sum
+        return n*(n+1)//2-sum"""
+        hashset=[0]*(len(nums)+1)
+        for num in nums:
+            hashset[num]+=1
+        return hashset.index(0)
         
