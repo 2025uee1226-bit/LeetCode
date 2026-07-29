555e6000118904eee9fc9747bc5462ecb61df3ac
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-29 23:53:28 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.15%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,7 +3,7 @@ class Solution(object):
         """
         :type digits: List[int]
         :rtype: List[int]
-        """
+        
         p=0
         u=[]
         d=len(digits)
@@ -14,5 +14,15 @@ class Solution(object):
         while p>0:
             u.append(p%10)
             p=p//10
-        return u[::-1]
-        
+        return u[::-1]"""
+       
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]!=9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+digits
+            
+
+
+
