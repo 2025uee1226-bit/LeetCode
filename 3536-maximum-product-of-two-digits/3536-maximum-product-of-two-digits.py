759ddefb113008df98d910bbd661de1f52ba9fc7
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-30 22:24:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.06%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -3,13 +3,24 @@ class Solution(object):
         """
         :type n: int
         :rtype: int
-        """
+        
         p,z=0,[]
         while(n>0):
             p=n%10
             z.append(p)
             n=n//10
         z.sort()
-        return z[-1]*z[-2]
+        return z[-1]*z[-2]"""
+        mx=0
+        smx,p=0,0
+        while(n>0):
+            p=n%10
+            if p>mx:
+                smx=max(mx,smx)
+                mx=p
+            else:
+                smx=max(p,smx)
+            n=n//10
+        return smx*mx
 
         
