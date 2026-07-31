0b4a22c748c00c15ab8a07e82ce345efe1ed1fad
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-31 17:57:47 +0530

    Time: 3 ms (47.07%), Space: 12.3 MB (88.62%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -7,13 +7,14 @@ class Solution(object):
         p=""
         z=""
         s=set()
-        for j in range(len(nums)):
-            for i in range(j+1,len(nums)):
-                p=nums[i].upper()
-                z=nums[i].lower()
-                if p in nums and z in nums:
-                    s.add(z)
-                    
+        
+        for i in range(0,len(nums)):
+            p=nums[i].upper()
+            z=nums[i].lower()
+            if p in nums and z in nums:
+                s.add(z)
+            
+                
         return len(s)
             
         
