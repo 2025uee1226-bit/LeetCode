c7b4fa3436972dd44ce11a4df2acbe62f8b99182
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-07-31 18:00:40 +0530

    Time: 0 ms (100%), Space: 12.6 MB (45.9%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
-        """
+        
         for i in range(len(nums)):
             if nums[i]==target:
                     return i
+        return -1"""
+        for num in nums:
+            if num==target:
+                return nums.index(target)
         return -1
         
