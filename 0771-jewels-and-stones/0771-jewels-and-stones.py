f334f2158be423c473bd9575d0330e8ec1caee1e
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-08-04 20:02:54 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.37%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -4,10 +4,15 @@ class Solution(object):
         :type jewels: str
         :type stones: str
         :rtype: int
-        """
+        
         count=0
         st=Counter(stones)
         for ch in jewels:
             if ch in stones:
                 count+=st[ch]
+        return count"""
+        count=0
+        for ch in jewels:
+            if ch in stones:
+                count+=stones.count(ch)
         return count
