fea78899d79c50579933b0cc9f221a5c8b565528
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-08-05 21:15:31 +0530

    Time: 0 ms (100%), Space: 12.5 MB (22.89%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
@@ -2,11 +2,13 @@ class Solution(object):
     def smallestEqual(self, nums):
         """
         :type nums: List[int]
-        :rtype: int
-        """
+        :rtype: int"""
+    
         for i in range(len(nums)):
             if i%10==nums[i]:
                 return i
                 break
         return -1
+
+
         
