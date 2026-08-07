d0ac1d9212b43858dc0c3f02eaaf95ec0c3ab4a0
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-08-07 13:10:48 +0530

    Time: 567 ms (0.71%), Space: 28.4 MB (0.97%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        for i in range(1,10**6):
+        for i in range(2,10**6,2):
             if i%2==0 and i%n==0:
                 return i
                 
