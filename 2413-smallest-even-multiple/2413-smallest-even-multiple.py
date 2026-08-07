0f8091f7141d2c7e1f9891c5ccee890ff82c0636
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-08-07 13:09:55 +0530

    Time: 1883 ms (0.71%), Space: 44.1 MB (0.97%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        for i in range(1,10**6):
+            if i%2==0 and i%n==0:
+                return i
+                break
+        
