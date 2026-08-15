32b15313d234775fe3201325da19ba768be80ad4
2025uee1226-bit <2025uee1226@mnit.ac.in>
2026-08-15 18:02:20 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.91%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -3,10 +3,18 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[int]
-        """
+        
         lst=[]
         count=Counter(nums)
         for num in nums:
             if count[num]>1:
                 lst.append(num)
-        return list(set(lst))
+        return list(set(lst))"""
+        s=set()
+        s1=[]
+        for num in nums:
+            if num not in s:
+                s.add(num)
+            else:
+                s1.append(num)
+        return s1
